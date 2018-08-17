6b1627eb3e3d0fc5f7dfd86c88c786c41f25a338
Roie Cohen <roie.cohen@gmail.com>
2018-08-16 23:35:42 -0700

    init

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-const cors = require('cors')
+
 express()
-  .use(cors)
+  // .use(cors)
+  .use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept'
+    )
+    next()
+  })
   .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`))
