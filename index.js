008d8664a561ae27193f65843a26628113ae65e6
Roie Cohen <roie.cohen@gmail.com>
2018-08-16 23:28:24 -0700

    init

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
+const cors = require('cors')
 express()
+  .use(cors)
   .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .listen(PORT, () => console.log(`Listening on ${PORT}`))
